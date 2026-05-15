5f7816140df0726820bce12ded4c7f4f820e31d4
chrisp <cpwildstar1000@gmail.com>
2026-05-15 14:53:23 -0500

    Fixed nic driver version variable

--- VA_Scripts/HPminiUpdate.ps1
+++ VA_Scripts/HPminiUpdate.ps1
@@ -75,67 +75,75 @@ if ($DNSComputerName -eq $FullComputerName) {
             if (Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Test-Path C:\DriverInstallFiles}) {
                 "Folder C:\DriverInstallFiles was created" | Tee-Object $LogFile -Append | Write-Host
                 foreach ($Driver in $DriverFiles) {
-                    Copy-Item -Path "\\mad-wsitbob\C$\Shares\Shared\Drivers\HP\HP 600 G6 Mini\$Driver.exe" -Destination "\\$FulComuterName\C$\DriverInstallFiles\$Driver.exe"
+                    Copy-Item -Path "\\mad-wsitbob\C$\Shares\Shared\Drivers\HP\HP 600 G6 Mini\$Driver.exe" -Destination "\\$FullComputerName\C$\DriverInstallFiles\$Driver.exe"
+                    "Copied $Driver to $FullComputerName" | Tee-Object $LogFile -Append | Write-Host
                 }
             }
 
             # Get current driver versions and output them to the log file
             $PreUpdateBIOS = Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Get-WMIObject Win32_BIOS | Select-Object SMBIOSBIOSVersion}
-            (($PreUpdateBIOS) -split "0")[1] | Tee-Object $LogFile -Append | Write-Host
+            $BIOSTest = (($PreUpdateBIOS) -split "0")[1]
+            if ($BIOSTest -lt "2.24.") {"Please update BIOS on device" | Tee-Object $LogFile -Append | Write-Host}
             
             "Intel(R) Wireless Bluetooth (R)" | Tee-Object $LogFile -Append | Write-Host
-            $PreUpdateBluetoothDriver = Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Get-WMIObject Win32_PNPSignedDriver -Filter "Description='Intel(R) Wireless Bluetooth (R)'" | Select-Object DriverVersion | ft}
+            $PreUpdateBluetoothDriver = Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Get-WMIObject Win32_PNPSignedDriver -Filter "Description='Intel(R) Wireless Bluetooth (R)'"}
             $PreUpdateBluetoothDriver.DriverVersion | Tee-Object $LogFile -Append | Write-Host
 
             "Intel(R) Management Engine WMI Provider" | Tee-Object $LogFile -Append | Write-Host
-            $PreUpdateMGMTDriver = Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Get-WMIObject Win32_PNPSignedDriver -Filter "Description='Intel(R) Management Engine WMI Provider'" | Select-Object DriverVersion}
+            $PreUpdateMGMTDriver = Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Get-WMIObject Win32_PNPSignedDriver -Filter "Description='Intel(R) Management Engine WMI Provider'"}
             $PreUpdateMGMTDriver.DriverVersion | Tee-Object $LogFile -Append | Write-Host
             
             "Intel(R) Ethernet Connection (11) I219-LM" | Tee-Object $LogFile -Append | Write-Host
-            $PreUpdateEthernetDriver = Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Get-WMIObject Win32_PNPSignedDriver -Filter "Description='Intel(R) Ethernet Connection (11) I219-LM'" | Select-Object DriverVersion}
-            $PreUpdateEthernetDriver.DrvierVersion | Tee-Object $LogFile -Append | Write-Host
+            $PreUpdateEthernetDriver = Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Get-WMIObject Win32_PNPSignedDriver -Filter "Description='Intel(R) Ethernet Connection (11) I219-LM'"}
+            $PreUpdateEthernetDriver.DriverVersion | Tee-Object $LogFile -Append | Write-Host
 
             "Intel(R) Wi-Fi 6 AX201 160MHz" | Tee-Object $LogFile -Append | Write-Host
-            $PreUpdateWiFiDriver = Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Get-WMIObject Win32_PNPSignedDriver -Filter "Description='Intel(R) Wi-Fi 6 AX201 160MHz'" | Select-Object DriverVersion}
+            $PreUpdateWiFiDriver = Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Get-WMIObject Win32_PNPSignedDriver -Filter "Description='Intel(R) Wi-Fi 6 AX201 160MHz'"}
             $PreUpdateWiFiDriver.DriverVersion | Tee-Object $LogFile -Append | Write-Host
 
             "Intel(R) UHD Graphics 630" | Tee-Object $LogFile -Append | Write-Host
-            $PreUpdateGraphicsDriver = Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Get-WMIObject Win32_PNPSignedDriver -Filter "Description='Intel(R) UHD Graphics 630'" | Select-Object DriverVersion}
+            $PreUpdateGraphicsDriver = Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Get-WMIObject Win32_PNPSignedDriver -Filter "Description='Intel(R) UHD Graphics 630'"}
             $PreUpdateGraphicsDriver.DriverVersion | Tee-Object $LogFile -Append | Write-Host
 
             "Realtek High Definition Audio" | Tee-Object $LogFile -Append | Write-Host
-            $PreUpdateAudioDriver = Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Get-WMIObject Win32_PNPSignedDriver -Filter "Description='Realtek High Definition Audio'" | Select-Object DrvierVersion}
+            $PreUpdateAudioDriver = Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Get-WMIObject Win32_PNPSignedDriver -Filter "Description='Realtek High Definition Audio'"}
             $PreUpdateAudioDriver.DriverVersion | Tee-Object $LogFile -Append | Write-Host
 
 
             # Run installers on remote computer
             Invoke-Command -ComputerName $FullComputerName -ScriptBlock {& 'C:\DriverInstallFiles\INTEL BLUETOOTH.exe' /s}
+            "Ran Bluetooth driver update" | Tee-Object $LogFile -Append | Write-Host
             Start-Sleep -Seconds 30
             Invoke-Command -ComputerName $FullComputerName -ScriptBlock {& 'C:\DriverInstallFiles\INTEL MGMT DRIVERS.exe' /s}
+            "Ran MGMT driver update" | Tee-Object $LogFile -Append | Write-Host
             Start-Sleep -Seconds 30
             Invoke-Command -ComputerName $FullComputerName -ScriptBlock {& 'C:\DriverInstallFiles\INTEL NIC DRIVER.exe' /s}
+            "Ran NIC driver update" | Tee-Object $LogFile -Append | Write-Host
             Start-Sleep -Seconds 30
             Invoke-Command -ComputerName $FullComputerName -ScriptBlock {& 'C:\DriverInstallFiles\INTEL THUNDERBOLT DRIVER.exe' /s}
+            "Ran Thunderbolt driver update" | Tee-Object $LogFile -Append | Write-Host
             Start-Sleep -Seconds 30
             Invoke-Command -ComputerName $FullComputerName -ScriptBlock {& 'C:\DriverInstallFiles\INTEL VIDEO AND CONTROL PANEL.exe' /s}
+            "Ran Graphics driver update" | Tee-Object $LogFile -Append | Write-Host
             Start-Sleep -Seconds 30
             Invoke-Command -ComputerName $FullComputerName -ScriptBlock {& 'C:\DriverInstallFiles\REALTEK AUDIO DRIVER.exe' /s}
+            "Ran Audio driver update" | Tee-Object $LogFile -Append | Write-Host
             Start-Sleep -Seconds 30
             Invoke-Command -ComputerName $FullComputerName -ScriptBlock {& 'C:\DriverInstallFiles\WIRELESS LAN DRIVERS.exe' /s}
-            Start-Sleep -Seconds 30
-            Invoke-Command -ComputerName $FullComputerName -ScriptBlock {& 'C:\DriverInstallFiles\INTEL BLUETOOTH.exe' /s}
+            "Ran LAN driver update" | Tee-Object $LogFile -Append | Write-Host
             Start-Sleep -Seconds 30
 
-            if ($CurrentBIOSVerion -lt '2.24.') {
+            <#if ($PreUpdateBIOS -lt '2.24.') {
                 Invoke-Command -ComputerName $FullComputerName -ScriptBlock {& 'C:\DriverInstallFiles\BIOS HP S22 Ver.02.24.00 Rev.A, 12-8-2025 sp166136.exe' /s}
+                "Ran BIOS update" | Tee-Object $LogFile -Append | Write-Host
                 Start-Sleep -Seconds 30
-            }
+            }#>
 
             # Wait while installing wireless driver to continue until computer is responding again
             While (!(Test-Connection $FullComputerName)) {Write-Host "Waiting for computer to start connect. If waiting for long period, check computer."}
             # Confirm driver installs are matching versions
-            $PostUpdateBIOS = Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Get-WMIObject Win32_BIOS | Select-Object SMBIOSBIOSVersion}
-            (($PostUpdateBIOS) -split "0")[1] | Tee-Object $LogFile -Append | Write-Host
+            #$PostUpdateBIOS = Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Get-WMIObject Win32_BIOS | Select-Object SMBIOSBIOSVersion}
+            #(($PostUpdateBIOS) -split "0")[1] | Tee-Object $LogFile -Append | Write-Host
             
             "Intel(R) Wireless Bluetooth (R)" | Tee-Object $LogFile -Append | Write-Host
             $PostUpdateBluetoothDriver = Invoke-Command -ComputerName $FullComputerName -ScriptBlock {Get-WMIObject Win32_PNPSignedDriver -Filter "Description='Intel(R) Wireless Bluetooth (R)'" | Select-Object DriverVersion | ft}
