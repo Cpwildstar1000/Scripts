56917b6be3522623f64f99d2fa85b3f0a1cd12d8
chrisp <cpwildstar1000@gmail.com>
2026-05-15 16:15:09 -0500

    File Move of Game Server Scripts

--- ArkServerUpdate.sh
+++
@@ -1,4 +0,0 @@
-tmux kill-session -t ArkServer
-cd /usr/lib/games/steam
-steamcmd +force_install_dir=/home/steam/Steam/steamapps/common/7d2d +login anonymous +app_update 376030 validate +quit
-tmux new-session -d -s ArkServer '/home/steam/ArkServerStart.sh'
